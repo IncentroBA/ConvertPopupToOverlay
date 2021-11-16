004d51d2409b63c3fc83f1415cfab2e6302e99f9
iamrob <rob.duits@incentro.com>
2021-11-16 13:30:17 +0100

    Add custom close action

--- src/ConvertPopupToOverlay.jsx
+++ src/ConvertPopupToOverlay.jsx
@@ -3,7 +3,14 @@ import "./ui/ConvertPopupToOverlay.css";
 import { createElement, useEffect, useState } from "react";
 import { waitFor } from "./helpers/waitFor";
 
-export default function ConvertPopupToOverlay({ closeButtonClass, closePage, position, size, showHeader }) {
+export default function ConvertPopupToOverlay({
+    closeButtonClass,
+    closeAction,
+    shouldClosePage,
+    position,
+    size,
+    showHeader
+}) {
     const [canRender, setCanRender] = useState(false);
     const [modal, setModal] = useState(null);
 
@@ -28,14 +35,16 @@ export default function ConvertPopupToOverlay({ closeButtonClass, closePage, pos
             const modal = document.querySelector(".popup-overlay");
             modal && modal.classList.remove("visible");
             document.body.classList.remove("popup-overlay-noscroll");
+            removeUnderlay();
         }
 
         function closeModal() {
             AnimateCloseModal();
-            removeUnderlay();
-            if (closePage === true) {
-                const modal = document.querySelector(".popup-overlay");
-                const closeBtn = modal.querySelector(".close");
+
+            if (closeAction && closeAction.canExecute) {
+                closeAction.execute();
+            } else if (!closeAction && shouldClosePage === true) {
+                const closeBtn = document.querySelector(".popup-overlay .close");
                 setTimeout(() => closeBtn.click(), 300);
             }
         }
@@ -49,25 +58,23 @@ export default function ConvertPopupToOverlay({ closeButtonClass, closePage, pos
 
             modal.insertAdjacentHTML("beforeend", '<div class="popup-underlay"></div>');
             const underlay = document.querySelector(".popup-underlay:not(.old)");
-            underlay.addEventListener("click", closeModal);
-            underlay.classList.add("old");
+            underlay && underlay.addEventListener("click", closeModal);
+            underlay && underlay.classList.add("old");
             return underlay;
         }
 
         // overlay for the default close button
         function generateCloseBtn() {
-            if (showHeader === true && closePage === true) {
+            if (showHeader === true && shouldClosePage === true) {
                 const modalContent = modal.querySelector(".modal-content");
-                modalContent.insertAdjacentHTML(
-                    "afterbegin",
-                    `<div class="popup-overlay__closebutton ${closeButtonClass}"></div>`
-                );
+                modalContent.insertAdjacentHTML("afterbegin", `<div class="popup-overlay__closebutton"></div>`);
+                document.querySelector(".popup-overlay__closebutton").addEventListener("click", closeModal);
             }
         }
-        
+
         function linkCloseButtons() {
             document.querySelectorAll(`.${closeButtonClass}`).forEach(closeBtn => {
-                if (closePage === true) {
+                if (shouldClosePage === true) {
                     closeBtn.addEventListener("click", closeModal);
                 } else {
                     closeBtn.addEventListener("click", AnimateCloseModal);
@@ -76,7 +83,7 @@ export default function ConvertPopupToOverlay({ closeButtonClass, closePage, pos
         }
 
         modal.classList.add("popup-overlay", `popup-overlay--${position}`);
-        
+
         setTimeout(() => {
             // Set size as width
             if (position === "left" || position === "right") {
@@ -86,19 +93,20 @@ export default function ConvertPopupToOverlay({ closeButtonClass, closePage, pos
             if (position === "top" || position === "bottom") {
                 modal.style.height = `${size}px`;
             }
-        }, 100); 
-        
+        }, 100);
+
         // Show/hide overlay header
         if (showHeader === false) {
             modal.classList.add("popup-overlay--remove-header");
         }
 
         document.body.classList.add("popup-overlay-noscroll");
-        
+
         // Wait with transitions in case of progressbar
         function foundProgress() {
             return true;
         }
+
         const progress = waitFor(".mx-progress", foundProgress, document);
         if (progress) {
             underlay.classList.remove("visible");
@@ -109,9 +117,9 @@ export default function ConvertPopupToOverlay({ closeButtonClass, closePage, pos
                 const underlay = generateUnderlay();
                 generateCloseBtn();
                 linkCloseButtons();
-                setTimeout(() => underlay.classList.add("visible"), 300);
-                setTimeout(() => modal.classList.add("transition"), 300);
-                setTimeout(() => modal.classList.add("visible"), 300);
+                setTimeout(() => underlay && underlay.classList.add("visible"), 300);
+                setTimeout(() => modal && modal.classList.add("transition"), 300);
+                setTimeout(() => modal && modal.classList.add("visible"), 300);
             }, 300);
         }
 
